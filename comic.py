e95ad8d27460166cbbb2c7df891b86967d38701f
Li-Kane <122249106+Li-Kane@users.noreply.github.com>
2023-08-23 22:13:26 -0700

    Comic Class
    Created Comic class which will search the intended manga for it's chapters and give url for chapters in the intended range, including decimal

--- comic.py
+++ comic.py
@@ -1,6 +1,116 @@
+import re, os, requests, bs4
+
 class Comic():
-    def __init__(self, website, range, path, link):
-        self.website = website
-        self.range = range
+    def __init__(self):
+        pass
+        
+    def setWebsite(self, website):
+        website = website.strip()
+        if(website != "1" and website != "2"):
+            print("Website input should be 1 or 2")
+            return True
+        if(website == "1"): 
+            self.website = "Mangakatana"
+        else:
+            self.website = "MangaKakalot"
+        return False
+    
+    def setRange(self, range):
+        pattern = r"((\d*\.)?\d+)-((\d*\.)?\d+)"
+        match = re.match(pattern, range)
+        if(not match):
+            print("Range input should be in form num1-num1, ex. 2-5")
+            return True
+        elif(float(match[1]) > float(match[3])):
+            print("2nd number in range should be bigger than the first")
+            return True
+        else:
+            self.range = [match[1], match[3]]
+        return False
+    
+    def setPath(self, path):
+        try: 
+            os.makedirs(path, exist_ok=True)
+        except:
+            print("Path cannot be made")
+            return True
         self.path = path
+        return False
+    
+    def setLink(self, link):
+        try: 
+            res = requests.get(link)
+        except requests.exceptions.MissingSchema:
+            print("Does not appear to be a link, try again!")
+            return True
+        if(not requests.codes.ok):
+            print("Cannot connect to link, check and try again")
+            return True
+        if("manganato" in link):
+            self.website = "Chapmanganato"
+        #try:
+        #   self.getTitle(res)
+        #    self.getChapters(res)
+        #except:
+        #   print("Cannot get chapters, check and try again")
+        #    return True
+        self.getTitle(res)
+        self.getChapters(res)
         self.link = link
+        return False
+    
+    def getChapters(self, res):
+        res.raise_for_status()
+        soup = bs4.BeautifulSoup(res.text, features='html.parser')
+        #find Chapters
+        if(self.website == "Mangakatana"):
+            chapter = soup.select('.chapters .chapter a')
+        elif(self.website == "Chapmanganato"):
+            chapter = soup.select('.row-content-chapter .a-h a')
+        else:
+            chapter = soup.select('.chapter-list .row a')
+        smallPattern = r"Chapter 0*" + self.range[0]
+        bigPattern = r"Chapter 0*" + self.range[1]
+        numPattern = r" ((\d*\.)?\d+)"
+        smallNumIdx = [idx for idx, item in enumerate(chapter) if re.search(smallPattern, item.string)]
+        bigNumIdx = [idx for idx, item in enumerate(chapter) if re.search(bigPattern, item.string)]
+        if(smallNumIdx == [] or bigNumIdx == []):
+            print("Range bounds are not within the manga")
+            raise IndexError
+        if(bigNumIdx[-1] == 0):
+            self.chapters = chapter[smallNumIdx[-1]::-1]
+        else:
+            self.chapters = chapter[smallNumIdx[-1]:bigNumIdx[-1]-1:-1]
+        #get a list of numbers associated with each chapter obj in chapters
+        chapNums = [re.search(numPattern, item.string) for item in self.chapters]
+        self.chapNums = [item.group(1) for item in chapNums]
+        return False
+    
+    def getTitle(self, res):
+        res.raise_for_status()
+        soup = bs4.BeautifulSoup(res.text, features='html.parser')
+        if(self.website == "Mangakatana"):
+            title = soup.select('#single_book .info .heading')
+        elif(self.website == "Chapmanganato"):
+            title = soup.select('.story-info-right h1')
+        else:
+            title = soup.select('.manga-info-top .manga-info-text h1')
+        self.title = title[0].string
+
+    def setCombine(self, combine):
+        ifCombine = combine.strip().lower()
+        if(ifCombine == "yes" or ifCombine == "y"):
+            self.combine = True
+            return False
+        elif(ifCombine == "no" or ifCombine=="n"):
+            self.combine = False
+            return False
+        else:
+            print("Please respond with yes or no")
+            return True
+    
+    def print(self):
+        print("You are downloading %s from chapters %0.2f-%0.2f" % (self.title, float(self.range[0]), float(self.range[1])))
+        print("Its path is %s" % self.path)
+        #for item in self.chapters:
+        #    print(item.string)
--- testScraper.py
+++ testScraper.py
@@ -4,20 +4,23 @@
 import requests, os, bs4, threading, re
 import comic
 
-def downloadXkcd(startComic, endComic):
-    for urlNumber in range(startComic, endComic):
+def downloadManga(startComic, endComic, comicObj):
+    for number in range(startComic, endComic):
         #download page
-        print('Downloading page https://ww5.mangakakalot.tv/chapter/manga-kd987738/chapter-%s...' % (urlNumber))
-        res = requests.get('https://ww5.mangakakalot.tv/chapter/manga-kd987738/chapter-%s' % (urlNumber))
+        chapter = comicObj.chapters[number]
+        print("Downloading page %s" % (chapter.get('href')))
+        res = requests.get(chapter.get('href'))
         res.raise_for_status()
         soup = bs4.BeautifulSoup(res.text, features='html.parser')
-
-        noHomeChapter = "chapter%s" % urlNumber
-        os.makedirs('NoHome/' + noHomeChapter, exist_ok=True)
-
-
+        print("SOUP                   ")
+        print(soup)
+        os.makedirs('%s/%s/ch.%s' % (comicObj.path, comicObj.title, comicObj.chapNums[number]) , exist_ok=True)
         #find URL of img
-        comicElem = soup.select('#vungdoc img')
+        if(comicObj.website == "Mangakatana"):
+            selector = '#imgs .wrap_img img'
+        
+        comicElem = soup.select(selector)
+        print(comicElem)
         if comicElem == []:
             print('Could not find comic image.')
         else:
@@ -30,35 +33,55 @@ def downloadXkcd(startComic, endComic):
                 res.raise_for_status()
 
                 #save to xkcd
-                imageFile = open(os.path.join('NoHome/' + noHomeChapter, "%s.jpg" % i), 'wb')
+                imageFile = open(os.path.join('%s/%s/ch.%s/' + "%s.jpg" % (comicObj.path, comicObj.title, comicObj.chapNums[number], i)), 'wb')
                 for chunk in res.iter_content(100000):
                     imageFile.write(chunk)
                 imageFile.close()
                 i += 1
 
-#User Interface
+#Asks necessary questions
+comicData = comic.Comic()
 while(True):
     print("Type 1 if you are downloading from MangaKatana and 2 if from MangaKakalot")
     website = input()
-    if(website.strip() != "1" and website.strip() != "2"):
-        print("Input format wrong, let's try that again!")
-        continue
-    print("Which chapters do you want to download? Give in form num1-num2, ex. 1-20")
-    range = input()
-    if(re.search("\d+-\d+", range.strip())==None):
-        print("Input format wrong, let's try that again!")
-        continue
-    numbers = re.findall("\d+", range.strip())
-    if(int(numbers[1]) < int(numbers[0])):
-        print("Range is not reasonable, let's try that again!")
-        continue
-    print("Please give the path to download the manga, or just the foldername if in the current working directory")
+    if(comicData.setWebsite(website)): continue
+    print("Which chapters do you want to download? Give in form num1-num2, ex. 1-20 or 2.5-15.1")
+    chpRange = input()
+    if(comicData.setRange(chpRange)): continue
+    print("Please give the path to place the manga folder, as an absolute or relative path (it will overwrite folders)")
     path = input()
+    if(comicData.setPath(path)): continue
     print("Please give the link to the manga. Ex. https://mangakatana.com/manga/the-horizon.25833")
     link = input()
-    comicData = comic.Comic(website, range, path, link)
-    print("Ok nice")
+    if(comicData.setLink(link)): continue
+    comicData.print()
+    print("Do you want to merge images of a chapter together?")
+    merge = input()
+    if(comicData.setCombine(merge)): continue
+    break
 
+#Set variables and start download threads
+downloadThreads = []
+size = int(len(comicData.chapters))
+if(size < 10):
+    stepSize = 2
+elif(size < 25):
+    stepSize = 5
+elif(size < 50):
+    stepSize = 10
+elif(size < 100):
+    stepSize = 20
+else:
+    stepSize = int(size/5)
 
-os.makedirs('NoHome', exist_ok=True)
+for i in range(0,size,stepSize):
+    pass
+    downloadThread = threading.Thread(target = downloadManga, args=(i, i+stepSize, comicData))
+    downloadThreads.append(downloadThread)
+    downloadThread.start()
+
+#wait for all threads to end.
+for downloadThread in downloadThreads:
+    downloadThread.join()
+print('Done.')
 
